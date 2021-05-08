4abb8859ffdc5fd06983572310e35617e5f02cac
Christian Hall <christiannealhall@gmail.com>
2021-05-08 08:38:27 -0400

    database 1.0

--- Data.sql
+++ Data.sql
@@ -2,27 +2,66 @@ DROP DATABASE IF EXISTS cycletrak;
 CREATE DATABASE cycletrak;
 USE cycletrak;
 
-CREATE TABLE user(
-	id				int				not null auto_increment,
+CREATE TABLE users(
+	id				int				primary key auto_increment,
     name			varchar(50)		not null,
     password		varchar(10)		not null,
     birthdate		date			not null,
     sex				varchar(1)		not null,
-    weight			decimal			not null,
+    weight			decimal(4,1)	not null,
     height			int				not null,
     total_mileage	bigint			not null,
     
     CONSTRAINT unique_username UNIQUE(name)
 );
 
-CREATE TABLE cycle(
-	id				int				not null auto_increment,
+CREATE TABLE cycles(
+	id				int				primary key auto_increment,
     name			varchar(15)		not null,
     bike_type		varchar(20)		not null,
-    year			bigint			not null,
+    year			int				not null,
     make			varchar(20)		not null,
     model			varchar(30)		not null,
     date_bought		date			not null,
     date_retired	date,
-    odometer		bigint			not null
+    odometer		bigint			not null,
+    
+    CONSTRAINT unique_cylename UNIQUE(name)
 );
+
+CREATE TABLE locations(
+	id				int				primary key auto_increment,
+    name			varchar(25)		not null,
+    city			varchar(30)		not null,
+    distance		int				not null,
+    terrain			varchar(25 )		not null,
+    notes			varchar(3000)	not null,
+    
+    CONSTRAINT unique_locationname UNIQUE(name)
+);
+
+CREATE TABLE rides(
+	id				int				primary key auto_increment,
+    name			varchar(50)		not null,
+    cycle_id		int				not null,
+    location_id		int				not null,
+    datetime		datetime		not null,
+    duration		bigint			not null,
+    distance		decimal			not null,
+    
+    FOREIGN KEY (cycle_id) REFERENCES cycles(id),
+    FOREIGN KEY (location_id) REFERENCES locations(id)
+);
+
+INSERT INTO users (id, name, password, birthdate, sex, weight, height, total_mileage) VALUES
+	(0, 'Christian Hall', 'June8th!', '1995-03-26', 'm', 197.6, 69, 29);
+
+INSERT INTO cycles (id, name, bike_type, year, make, model, date_bought, odometer) VALUES
+	(0, 'ESPORTA Bike', 'Stationary', '2015', 'Life-Fitness', 'C1 Upright', '2021-03-31', 29);
+
+INSERT INTO locations (id, name, city, distance, terrain, notes) VALUES
+	(0, 'ESPORTA Oakley', 'Cincinnati', 0, 'Stationary', 'Stationary bike at my local gym');
+
+-- INSERT INTO rides (id, name, cycle_id, location_id, datetime, duration, distance) VALUES
+-- 	(0, 'Proving Ride 1', 0, 0, '2021-05-03 16:15:00', 3600, 14.7);
+--  (1, 'Priving Ride 2', 0, 0, '2021-05-05 16:15:00', 3600, 14.3);
--- ride.py
+++ ride.py
@@ -2,12 +2,11 @@ from datetime import datetime
 
 
 class Ride:
-    def __init__(self, ride_id, ride_name, bike, location, date, time, duration, distance):
+    def __init__(self, ride_id, ride_name, bike, location, datetime, duration, distance):
         self.ride_id = ride_id
         self.ride_name = ride_name
         self.bike = bike
         self.location = location
-        self.date = date
-        self.time = time
+        self.datetime = datetime
         self.duration = duration
         self.distance = distance
